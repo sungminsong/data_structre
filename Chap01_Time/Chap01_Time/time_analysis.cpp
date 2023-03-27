3dbfead80eefded1c4af2082daff13fef09e9fa4
sungminsong <tjdlas779@naver.com>
2023-03-27 12:42:06 +0900

    code modified

--- Chap01_Time/Chap01_Time/time_analysis.cpp
+++ Chap01_Time/Chap01_Time/time_analysis.cpp
@@ -2,6 +2,21 @@
 #include <cstdlib>
 #include <ctime>
 
+
+int sum_of_N(int n)
+{
+	int sum = 0;
+	for (int i = 0; i < n + 1; i++) {
+		sum = sum + i;
+	}
+	return sum;
+}
+
+int sum_of_N2(int n)
+{
+	return (n * (n + 1)) / 2;
+}
+
 int main()
 {
 	clock_t start, finish;  // 시작 시각, 끝나는 시각
@@ -9,7 +24,11 @@ int main()
 
 	start = clock();		// 현재 시각을 start에 저장
 	// 실행시간을 측정하고자 하는 코드 입력
-	int a = 10 + 20;
+	// sum_of_N 함수 호출
+	//int sum = sum_of_N(100000000);
+
+	// sum_of_N2 함수 호출
+	 int sum_of_N2(100000000);
 	// ...
 	finish = clock();
 
